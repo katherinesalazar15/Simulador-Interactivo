6219b51458eb0c7cc958c7143d97a0797d6b0436
Katherine Salazar <katty@inipop.com>
2022-07-11 18:54:10 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+function agregarAlCarrito(productos) {
+
+  
+
+  let productosAgregados = "";
+
+    
+    for(let posicion = 0; posicion < 3; posicion++){
+
+      productosAgregados += posicion;
+
+        console.log("Agregar el producto al carrito: " + productos);
+
+    }
+   
+    
+}
+
+agregarAlCarrito('Pollera Sofia');
+agregarAlCarrito('Remera Umma');  
+agregarAlCarrito('Pantalón Berlin');
+
+
+
+
+
+
